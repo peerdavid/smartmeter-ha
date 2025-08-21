d403a51041f20e136dafd9292496b70178aec032
Helmut Wolf <3902045+shadow1runner@users.noreply.github.com>
2025-08-21 12:39:04 +0200

    Upgrade to `python:3-slim` base image (now using bookworm)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3-slim-buster
+FROM python:3-slim
 
 WORKDIR /usr/src/app
 
--- tools/setup.sh
+++ tools/setup.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+#! /usr/bin/env sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
 
 # Install everything needed for the smart-meter connection and our app
-sudo apt-get udpate
+sudo apt-get update
 sudo apt-get upgrade -y
 
 # Setup docker
 curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER # requires logout and login to take effect
 
 # sudo apt-get install python3-lxml -y
 
