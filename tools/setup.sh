f8984753b196d4fb2c7e16fed4a8400c7288a1b3
Helmut Wolf <3902045+shadow1runner@users.noreply.github.com>
2025-08-21 12:39:04 +0200

    Working version

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,7 +2,13 @@
 
 # Install everything needed for the smart-meter connection and our app
 sudo apt-get udpate
-sudo apt-get install python3-lxml -y
+sudo apt-get upgrade -y
+
+# Setup docker
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+
+# sudo apt-get install python3-lxml -y
 
 # Optionally install an mqtt broker
 # sudo apt install mosquitto -y
